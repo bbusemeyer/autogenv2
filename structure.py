3ca2a388583d1458a2121c3a76c4fb2530a56b78
Brian Busemeyer <bbusemeyer@gmail.com>
2018-06-14 10:50:16 -0500

    Added structure docs.

---
+++ structure.py
@@ -0,0 +1,10 @@
+class Structure:
+  ''' Class containing information:
+    - atom placement,
+    - atom types,
+    - pseudopotentials (if any),
+    - spin,
+    - k-points (if any),
+    - lattice vectors (if periodic).
+
+  Can also export to various file outputs.'''
